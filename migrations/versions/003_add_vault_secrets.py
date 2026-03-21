2ebe3ddb5e28e10598b44ed03e3fa17f5069874d
Alan Chen <zc2610@nyu.edu>
2026-03-21 11:16:58 -0400

    chore(migrations): renumber vault secrets migration from 002 to 003

--- migrations/versions/003_add_vault_secrets.py
+++ migrations/versions/003_add_vault_secrets.py
@@ -3,8 +3,8 @@
 Stores encrypted per-workspace secrets (API keys, credentials)
 that are injected into sandbox code execution.
 
-Revision ID: 002
-Revises: 001
+Revision ID: 003
+Revises: 002
 Create Date: 2026-03-20
 """
 
@@ -13,8 +13,8 @@ from typing import Sequence, Union
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "002"
-down_revision: Union[str, None] = "001"
+revision: str = "003"
+down_revision: Union[str, None] = "002"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
